55e57296ae0c5b52c09d9b9119c6e1338d2f1c01
Glitch (remiix-new-11ty) <none>
2020-12-03 01:43:19 +0000

    🚞🏈 Updated with Glitch

---
+++ README.md
@@ -0,0 +1,5 @@
+# Website
+
+New website for RemiixInc, made with 11ty.
+
+https://remiixinc.ml
--- _data/youtube.js
+++ _data/youtube.js
@@ -1,7 +1,7 @@
 var request = require("sync-request");
 var res = request(
   "GET",
-  `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${process.env.YOUTUBE_CHANNEL_ID}&key=${process.env.YOUTUBE_API_KEY}`,
+  `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCkuKzwbQX0WFG2v457QTOSw&key=${process.env.API_KEY}`,
   {
     headers: {
       "user-agent": "RemiixInc"
